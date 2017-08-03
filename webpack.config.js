54d6b5f4179b526f52cdd986492e9dbadda38d65
Liang Boan <liangboxan@gmail.com>
2017-08-03 23:58:36 +0800

    project init

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    devtool: 'inline-source-map',
+    entry: './src/index.ts',
+    output: {
+        path: __dirname + '/dist',
+        filename: 'bundle.js'
+    },
+    resolve: {
+        // Add `.ts` and `.tsx` as a resolvable extension.
+        extensions: ['.ts', '.tsx', '.js'] // note if using webpack 1 you'd also need a '' in the array as well
+    },
+    devServer: {
+        contentBase: './dist'
+    },
+    module: {
+        rules: [{
+            test: /\.less$/,
+            use: [{
+                loader: "style-loader" // creates style nodes from JS strings
+            }, {
+                loader: "css-loader" // translates CSS into CommonJS
+            }, {
+                loader: "less-loader" // compiles Less to CSS
+            }]
+        }, {
+            test: /\.tsx?$/,
+            loader: 'ts-loader'
+        }]
+    },
+    plugins: [new HtmlWebpackPlugin()]
+};
